7a97ac1f996d419f3320523062f281968eae620b
Jason Beres <jasonberes@gmail.com>
2024-12-06 17:55:23 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,83 @@
+using Reveal.Sdk;
+using Reveal.Sdk.Dom;
+using RevealSdk.Server;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers().AddReveal(builder =>
+{
+    builder
+        .AddDataSourceProvider<DataSourceProvider>();
+});
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.DocumentFilter<CustomDocumentFilter>();
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+      builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()
+    );
+});
+
+var app = builder.Build();
+
+app.UseCors("AllowAll");
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.MapGet("/dashboards/names", () =>
+{
+    try
+    {
+        string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "Dashboards");
+        var files = Directory.GetFiles(folderPath);
+        Random rand = new();
+
+        var fileNames = files.Select(file =>
+        {
+            try
+            {
+                return new DashboardNames
+                {
+                    DashboardFileName = Path.GetFileNameWithoutExtension(file),
+                    DashboardTitle = RdashDocument.Load(file).Title
+                };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error Reading FileData {file}: {ex.Message}");
+                return null;
+            }
+        }).Where(fileData => fileData != null).ToList();
+
+        return Results.Ok(fileNames);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error Reading Directory : {ex.Message}");
+        return Results.Problem("An unexpected error occurred while processing the request.");
+    }
+
+}).Produces<IEnumerable<DashboardNames>>(StatusCodes.Status200OK)
+.Produces(StatusCodes.Status404NotFound)
+.ProducesProblem(StatusCodes.Status500InternalServerError);
+
+
+app.Run();
+
+// ***
+// This is a helper class to store the dashboard names
+// ***
+public class DashboardNames
+{
+    public string? DashboardFileName { get; set; }
+    public string? DashboardTitle { get; set; }
+}
